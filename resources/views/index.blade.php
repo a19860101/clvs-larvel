2e731fb1ca74192de03f2992887b89b12f5c4b35
Zac <a19860101@gmail.com>
2025-04-17 15:40:25 +0800

    20250417

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,21 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        .container {
+            width: 1200px;
+            margin: auto;
+            display: flex;
+            flex-wrap: wrap;
+        }
+        .box {
+            width: 100%;
+            padding: 15px;
+            margin-bottom: 15px;
+            background-color: #ccc;
+        }
+        .box h3 {
+            margin-bottom: 15px;
+        }
+        .box .content {
+            margin-bottom: 15px;
+        }
+    </style>
 </head>
 <body>
     <a href="/post/create">新增資料</a>
-    <div>
+    <div class="container">
         @foreach($posts as $post)
-        <h3>{{$post->title}}</h3>
-        <div>
-            {{$post->content}}
+        <div class="box">
+
+            <h3>{{$post->title}}</h3>
+            <div class="content">
+                {{$post->content}}
+            </div>
+            <form action="/post/{{$post->id}}" method="post">
+                @csrf
+                @method('delete')
+                <input type="submit" value="刪除" onclick="return confirm('確認刪除?')">
+            </form>
         </div>
-        <form action="/post/{{$post->id}}" method="post">
-            @csrf
-            @method('delete')
-            <input type="submit" value="刪除" onclick="return confirm('確認刪除?')">
-        </form>
-        <hr>
+
         @endforeach
     </div>
 </body>
